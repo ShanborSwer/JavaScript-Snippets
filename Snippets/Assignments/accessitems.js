ec867f18e5d0efd0dd0dd9e9474ca6a7d25700ce
ShanborSwer <shanborswer98@gmail.com>
2024-02-06 11:50:03 +0530

    assign

---
+++ Snippets/Assignments/accessitems.js
@@ -0,0 +1,5 @@
+let names = ['Shanbor','Sunil','Rajesh','Ashish','Wahid']
+
+console.log(names[3]);
+
+//Access items using array index
---
+++ Snippets/Assignments/lastitem.js
@@ -0,0 +1,5 @@
+let names = ['Shanbor','Sunil','Rajesh','Ashish','Wahid']
+
+let last=names[names.length-1]
+
+console.log(last)
