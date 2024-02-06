dbea4b3d8891683317ed0d1b05f3de72133e1cb2
ShanborSwer <shanborswer98@gmail.com>
2024-02-06 12:40:48 +0530

    assign

---
+++ Snippets/Assignments/arrobjitem.js
@@ -0,0 +1,9 @@
+let emp = [
+    {id:101,name:'Shanbor',sal:45000},
+    {id:102,name:'Sunil',sal:55000},
+    {id:102,name:'Rajesh',sal:65000}
+]
+
+for (i=0;i<emp.length;i++){
+    console.log(emp[i])
+}
