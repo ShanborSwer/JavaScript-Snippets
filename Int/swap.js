0bdcbfbb62d0ad7277f2e622344f3f33030b8daf
ShanborSwer <shanborswer98@gmail.com>
2024-03-04 15:03:43 +0530

    int

---
+++ Int/swap.js
@@ -0,0 +1,21 @@
+/* var num1 = 2;
+var num2 = 3;
+var temp;
+
+num1 = num1+num2; // 2+3=5
+
+
+num2 = num1-num2;  //5-3=2
+num1 = num1-num2; // 5-2=3
+
+console.log(num1)  //3
+console.log(num2) //2 */
+
+var a = 10;
+var b = 20;
+var c;
+
+
+
+console.log(a)
+console.log(b)
---
+++ Int/swap1.js
@@ -0,0 +1,9 @@
+//using destructor
+
+let a = 2;
+let b = 3;
+
+[a,b] = [b,a];
+
+console.log(a)
+console.log(b)
